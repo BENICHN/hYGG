15d1c1c28507c2d45bab46dd30e2601f24c73b23
BENICHN <nathan.benichou31280@gmail.com>
2021-08-10 00:04:45 +0200

    ping

--- src/API.hs
+++ src/API.hs
@@ -18,6 +18,7 @@ decoResp = (setHeader "Access-Control-Allow-Origin" "*" >>)
 
 app :: Config -> API
 app c = do
+  get "ping" $ decoResp $ text "pong"
   get "search" $ decoResp $ do
     params >>= liftIO . parseSearchTorrents c >>= json
   get ("torrent" <//> wildcard) $ \s -> decoResp $ liftIO (parseTorrentInfos c $ unpack s) >>= json
